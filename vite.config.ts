8bf6890cff6dc1a1c08c329b7c2b6f8f008f8335
yyye55 <2467124911@qq.com>
2026-07-03 16:54:48 +0800

    冲突

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,7 @@
 import { defineConfig, loadEnv } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import tailwindcss from '@tailwindcss/vite'
-import { fileURLToPath, URL } from 'url'
+import { resolve } from 'path'
 
 export default defineConfig(({ mode }) => {
   const env = loadEnv(mode, process.cwd(), '')
